fe2d13149cc80a716d7adb07c432b026af9ad194
Gustavo Faroni <gustavofaroni@gmail.com>
2025-07-15 17:50:36 -0300

    dividido em pastas e preparando para dividir os scripts

--- scripts/listaAlunos.js
+++ scripts/listaAlunos.js
@@ -5,80 +5,110 @@ export const alunos = [
         nome: "Ana Clara Oliveira",
         sexo: "Feminino",
         dtNascimento: "2010-05-15",
-        grauEscolaridade: "9º Ano do Ensino Fundamental",
+        grauEscolaridade: "Superior Completo",
         endereco: "Rua das Flores, 123, Bloco A, Apto 101",
-        foto: "img/ana clara.png"
+        foto: "img/ana clara.png",
+        salario: 3500.00,
+        passagemDiaria: 8.60,
+        opcaoVT: true
     },
     {
         nome: "Lucas Martins Souza",
         sexo: "Masculino",
         dtNascimento: "2012-09-22",
-        grauEscolaridade: "7º Ano do Ensino Fundamental",
+        grauEscolaridade: "Ensino Médio Completo",
         endereco: "Avenida Principal, 45, Casa 2",
-        foto: "img/lucas martins.png"
+        foto: "img/lucas martins.png",
+        salario: 3500.00,
+        passagemDiaria: 17.10,
+        opcaoVT: true
     },
     {
         nome: "Beatriz Costa Lima",
         sexo: "Feminino",
         dtNascimento: "2008-02-10",
-        grauEscolaridade: "2º Ano do Ensino Médio",
+        grauEscolaridade: "Superior Completo",
         endereco: "Travessa dos Coqueiros, 88",
-        foto: "img/beatriz costa.png"
+        foto: "img/beatriz costa.png",
+        salario: 3300.00,
+        passagemDiaria: 17.10,
+        opcaoVT: false
     },
     {
         nome: "Pedro Almeida Santos",
         sexo: "Masculino",
         dtNascimento: "2009-11-30",
-        grauEscolaridade: "1º Ano do Ensino Médio",
+        grauEscolaridade: "Ensino Médio Completo",
         endereco: "Rua da Passagem, 765, Apto 302",
-        foto: "img/pedro almeida.png"
+        foto: "img/pedro almeida.png",
+        salario: 3500.00,
+        passagemDiaria: 8.60,
+        opcaoVT: false
     },
     {
         nome: "Júlia Ferreira Gomes",
         sexo: "Feminino",
         dtNascimento: "2014-07-18",
-        grauEscolaridade: "5º Ano do Ensino Fundamental",
+        grauEscolaridade: "Superior Completo",
         endereco: "Alameda dos Pássaros, 321",
-        foto: "img/julia ferreira.png"
+        foto: "img/julia ferreira.png",
+        salario: 4100.00,
+        passagemDiaria: 8.60,
+        opcaoVT: true
     },
     {
         nome: "Guilherme Pereira Rodrigues",
         sexo: "Masculino",
         dtNascimento: "2007-08-01",
-        grauEscolaridade: "3º Ano do Ensino Médio",
+        grauEscolaridade: "Superior Cursando",
         endereco: "Rua do Sol, 50, Fundos",
-        foto: "img/guilherme pereira.png"
+        foto: "img/guilherme pereira.png",
+        salario: 1700.00,
+        passagemDiaria: 17.60,
+        opcaoVT: true
     },
     {
         nome: "Sofia Ribeiro Carvalho",
         sexo: "Feminino",
         dtNascimento: "2011-03-25",
-        grauEscolaridade: "8º Ano do Ensino Fundamental",
+        grauEscolaridade: "Ensino Médio Completo",
         endereco: "Praça da Bandeira, 15, Apto 504",
-        foto: "img/sofia ribeiro.png"
+        foto: "img/sofia ribeiro.png",
+        salario: 3500.00,
+        passagemDiaria: 17.20,
+        opcaoVT: false
     },
     {
         nome: "Matheus Gonçalves Dias",
         sexo: "Masculino",
         dtNascimento: "2013-12-05",
-        grauEscolaridade: "6º Ano do Ensino Fundamental",
+        grauEscolaridade: "Superior Cursando",
         endereco: "Rua 25 de Março, 999",
-        foto: "img/matheus goncalves.png"
+        foto: "img/matheus goncalves.png",
+        salario: 1500.00,
+        passagemDiaria: 8.60,
+        opcaoVT: false
     },
     {
         nome: "Isabela Mendes Barbosa",
         sexo: "Feminino",
         dtNascimento: "2008-06-12",
-        grauEscolaridade: "2º Ano do Ensino Médio",
+        grauEscolaridade: "Superior Completo",
         endereco: "Avenida das Américas, 1200, Bloco C, Apto 201",
-        foto: "img/isabela mendes.png"
+        foto: "img/isabela mendes.png",
+        salario: 4300.00,
+        passagemDiaria: 8.60,
+        opcaoVT: false
     },
     {
         nome: "Rafael Castro e Silva",
         sexo: "Masculino",
         dtNascimento: "2009-01-07",
-        grauEscolaridade: "1º Ano do Ensino Médio",
+        grauEscolaridade: "Ensino Médio Completo",
         endereco: "Rua da Matriz, 22, Casa",
-        foto: "img/rafael-castro.png"
+        foto: "img/rafael-castro.png",
+        salario: 1860.00,
+        passagemDiaria: 17.20,
+        opcaoVT: true
     }
 ];
